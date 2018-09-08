067b3849981b8fa17e44e036aa0fb0f670abe034
chloele33 <chloele@seas.upenn.edu>
2018-09-07 22:40:52 -0400

    added face 2 to popup

--- popup/popup.js
+++ popup/popup.js
@@ -3,11 +3,19 @@ var currentFace = document.getElementById("currentFace");
 var image = document.createElement("img");
 image.setAttribute("src", "../assets/amyghappy.png");
 
+var image2 = document.createElement("img");
+image2.setAttribute("src", "../assets/amygsad.png");
+
 function setFace(path) {
   image.setAttribute("src", path);
+  image2.setAttribute("src", path);
 }
 
 image.setAttribute("width", "70");
 image.setAttribute("height", "auto");
+image2.setAttribute("width", "70");
+image2.setAttribute("height", "auto");
+
 
 currentFace.appendChild(image);
+currentFace.appendChild(image2);
