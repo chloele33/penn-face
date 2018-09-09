f668c3705b6278170a7a128d678fef1a1b8edc49
chloele33 <chloele@seas.upenn.edu>
2018-09-08 20:22:02 -0400

    attempt to read from local

--- content_scripts/penn-face.js
+++ content_scripts/penn-face.js
@@ -1,6 +1,42 @@
 var cursorURL1 = 'https://raw.githubusercontent.com/chloele33/penn-face/master/assets/amyghappy-cursor.png';
 var cursorURL2 = 'https://raw.githubusercontent.com/chloele33/penn-face/master/assets/amygsad-cursor.png';
 
+// var reader = new FileReader();
+
+// reader.onload = function(progressEvent){
+//     // Entire file
+//     console.log(this.result);
+
+//     // By lines
+//     var lines = this.result.split('\n');
+//     for(var line = 0; line < lines.length; line++){
+//       console.log(lines[line]);
+//   }
+// };
+
+//reader.readAsText("file:///assets/currentURL.txt");
+
+// function readTextFile(file)
+// {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.open("GET", file, false);
+//     rawFile.onreadystatechange = function ()
+//     {
+//         if(rawFile.readyState === 4)
+//         {
+//             if(rawFile.status === 200 || rawFile.status == 0)
+//             {
+//                 var allText = rawFile.responseText;
+//                 alert(allText);
+//             }
+//         }
+//     }
+//     rawFile.send(null);
+// }
+
+// readTextFile("file://../assets/currentURL.txt");
+
+
 $("body").css({'cursor': 'url(' + cursorURL1 + '), default'});
 
 var mouseDownFunction = function (e) {
